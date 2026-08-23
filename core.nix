dbc790d9cdadef04531572e218787f371268833b
Ichinose-Kazuki <mail@ichinose-kazuki.jp>
2026-08-23 19:36:44 +0900

    refactor: add HM-free core module with options and config build

---
+++ core.nix
@@ -0,0 +1,90 @@
+{ inputs }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.myNvim;
+
+  nixvimPkg = inputs.nixvim.legacyPackages.${pkgs.stdenv.hostPlatform.system}.makeNixvimWithModule {
+    inherit pkgs;
+    module = {
+      imports = [ ./nixvimConfig ];
+    };
+    extraSpecialArgs = {
+      inherit inputs;
+      myNvimCfg = cfg;
+    };
+  };
+in
+{
+  options.myNvim = {
+    enable = lib.mkEnableOption "my neovim configuration";
+
+    fileExplorer = lib.mkOption {
+      type = lib.types.enum [
+        "neo-tree"
+        "oil"
+      ];
+      default = "oil";
+      description = "File explorer plugin to use";
+    };
+
+    colorscheme = lib.mkOption {
+      type = lib.types.enum [
+        "vscode"
+        "tokyonight"
+        "catppuccin"
+      ];
+      default = "vscode";
+      description = "Colorscheme to use";
+    };
+
+    lsp.servers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "lua_ls"
+        "nil_ls"
+        "ts_ls"
+        "html"
+        "cssls"
+        "jsonls"
+        "pyright"
+        "clangd"
+        "bashls"
+        "yamlls"
+      ];
+      description = "LSP servers to enable";
+    };
+
+    plugins = {
+      git.enable = lib.mkEnableOption "git plugins (gitsigns, gitgraph)" // {
+        default = true;
+      };
+      zen.enable = lib.mkEnableOption "zen-mode and no-neck-pain plugins" // {
+        default = true;
+      };
+      dashboard.enable = lib.mkEnableOption "dashboard (alpha)" // {
+        default = true;
+      };
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      readOnly = true;
+      description = "The built neovim package (before wrapping).";
+    };
+
+    finalPackage = lib.mkOption {
+      type = lib.types.package;
+      default = cfg.package;
+      description = "The package that gets installed. Override this to wrap the package (e.g. sandboxing).";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    myNvim.package = nixvimPkg;
+  };
+}
